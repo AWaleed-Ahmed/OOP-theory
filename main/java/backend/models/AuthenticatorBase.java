7cb7083767f3481ad96a655cb4583562b5db9169
Waleed <iwaleedahmed05@gmail.com>
2025-05-28 13:41:40 +0500

    deleted files

--- main/java/backend/models/AuthenticatorBase.java
+++
@@ -1,7 +0,0 @@
-package backend.models;
-
-public abstract class AuthenticatorBase {
-    public abstract void signup(User user);
-    public abstract void loadUsersFromFile();
-    public abstract boolean authenticate(User user);
-}
